d9275aa5c8ac2521a86e0d0534b3d07474889e42
gawain <lvcoffman@gmail.com>
2021-05-08 00:32:54 -0400

    updated survey results

--- mentor_vis.py
+++ mentor_vis.py
@@ -37,7 +37,15 @@ if st.checkbox('Show Dataframe'):
 respondents = len(survey_bin['id'])
 start_date = survey_bin.iloc[0]['start_time'].split(' ',1)[0]
 
-st.write('This survey had ', respondents, ' respondents and was administered the week of ', start_date, '.')
+st.markdown('## Summary')
+st.write('This survey had ', respondents, ' respondents and was administered \
+    the week of ', start_date, '.  The average time for completion was 10 min.')
+
+st.write('Of the ', respondents, ' respondents, 24 ',\
+    ' would like to have a mentor.  Most respondents do not have a preference for a \
+    mentor inside or outside of engineering, but would like to gain a better understanding \
+    of Finance and RD&E.  The main topics of interest were Career Development followed by \
+    Influencing Others and Building your Brand.')
 
 # Do you have a current mentor?
 
@@ -46,7 +54,10 @@ current = current_df.plot(kind='pie', y='mentor_current', colormap=cmap, figsize
 current.set_title('Do you currently have a mentor?')
 current.set_ylabel(None)
 
-st.write('We asked respondents if they currently had a mentor.  ', current_df['Yes'], ' of the respondets replied "Yes" they have a mentor and ', current_df['No'], ' replied "No" they do not have a mentor currently.')
+st.markdown('### Current Mentor Status')
+st.write('We asked respondents if they currently had a mentor.  ', current_df['Yes'], \
+' of the respondets replied "Yes" they have a mentor and ', current_df['No'], \
+' replied "No" they do not have a current mentor. ')
 c1, c2, c3 = st.beta_columns((1,2,1))
 c2.pyplot()
 
@@ -54,7 +65,12 @@ c2.pyplot()
 
 ever_df = survey.groupby(['mentor_ever']).size()
 
-st.write('We were also curious if respondents had ever had a mentor.  ', ever_df['Yes'], 'respondents have had a previous mentor and ', ever_df['No'], ' have not had a previous mentor.')
+
+st.markdown('### Previous Mentor Status')
+st.write('We were also curious if respondents had ever had a mentor.  ', \
+    ever_df['Yes'], 'respondents have had a previous mentor and ', ever_df['No'], \
+    ' have not had a previous mentor.  The dark blue bars below are those that \
+    currently have a mentor and the light blue bar are those that do not have a current mentor.')
 c1,c2 = st.beta_columns(2)
 
 ever_df_no = survey[survey['mentor_ever']=='No'].groupby(['mentor_ever', 'mentor_current']).size()
@@ -64,7 +80,7 @@ ever_no = ever_df_no.unstack().plot(kind='bar', colormap=cmap)
 ever_no.set_ylim([0,18])
 ever_no.set_xlabel("")
 ever_no.legend(title='Current mentor')
-ever_no.annotate('confusion', xy=(0.15,4), xytext=(0.25,8), 
+ever_no.annotate('First time mentees', xy=(0.15,4), xytext=(0.2,8), 
         arrowprops=dict(facecolor='black', shrink=0.05))
 
 c1.header("Previous Mentor = Never")
@@ -83,26 +99,78 @@ ever_df = survey.groupby(['mentor_ever', 'mentor_current']).size()
 
 # interenst in having a mentor or having a second mentor
 
-st.header('Desire for a mentor')
 # TODO: add df groupby 'mentor_desired' and 'mentor_additional' show in a similar manner to that above. 
+
+desire = survey[survey['mentor_current']=='No'].groupby('mentor_desired').size()
+addnl = survey[survey['mentor_current']=='Yes'].groupby('mentor_additional').size()
+
+st.markdown('### Desire for a mentor:')
+st.write('Of the ', respondents, ' respondents ', desire['Yes']+addnl['Yes'], 'would like a mentor. \n \
+    \n Respondents were asked if they desired a mentor if they did not have one, \
+    or an additional mentor if they have current mentor.  Of the ', current_df['No'], \
+    ' respondents who do not have a mentor, ', desire['Yes'], ' would like to have a mentor. \
+    And of the ', current_df['Yes'], ' respondents who have a current mentor, ', addnl['Yes'], \
+    ' would like to have an additional mentor.')
+
+
 
 # Interest by topic
 
 # TODO: create pie chart by column
 
 st.header('Topics of Interest')
-st.write('We gave respondents a list of topics to choose from.  They were allowed to select as many topics as they liked.  Below you see those topics that recieved more than one vote. ')
+st.write('To understand what respondents would like to be mentored in, we gave \
+    respondents a list of topics to choose from.  They were allowed to select all \
+    topics that applied to them.  The below chart shows all options provided to \
+    respondents and the corresponding votes.   \n \
+    \n Career Development recieved the most votes with 25 of 34 respondents selecting this option.')
+
+
+
 topics_total = mentor_topics.iloc[34].copy()
 topics_total.drop(['Unnamed: 0','id'], inplace=True)
 
-topics_plot = topics_total.where(topics_total >1).plot(kind='pie', colormap=cmap)
-topics_plot.set_title(label='')
+
+topics_plot = topics_total.sort_values(ascending=True).plot(kind='barh', colormap=cmap)
+topics_plot.set_ylabel('')
 plt.show()
 st.pyplot()
 
-topics_total
 
 st.header('What has been learned from mentors in the past?')
 
+st.markdown('summary of responses')
+
+lesson = survey['mentor_benefits_str'].dropna()
+
+if st.checkbox('Show detailed responses for mentoring benefits'):
+    for response in lesson:
+        st.write(response)
 
 st.header('What function would you like your mentor to come from?')
+
+# TODO: remind desire for current mentor (just number)
+# TODO: bag of words / pie chart where people would like mentor to come from
+
+in_out = survey.groupby('mentor_in_out').size()
+functions = survey['mentor_function'].dropna()
+
+st.markdown('We asked respondents if they wanted a mentor from within the greater \
+    engineering organization or outside of engineering.  Most are open to either \
+    inside or outside of engineering.  \n \
+    \n For those that wanted a mentor outside of engineering, we asked what function \
+    they saught a mentor from. The top requests are **RD&E** and **Finance**, \
+    followed closely by Sales and Marketing with Enterprise Excellence and \
+    Executive Management getting an honerable mention. ')
+
+
+in_out_plot = in_out.plot(kind='pie', colormap=cmap)
+in_out_plot.set_ylabel('')
+c1, c2, c3 = st.beta_columns((1,2,1))
+c2.pyplot()
+
+
+
+if st.checkbox('Show detailed responses for mentor function'):
+    for response in functions:
+        st.write(response)
